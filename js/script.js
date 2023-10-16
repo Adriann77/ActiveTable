a590dbee986068e0580947611f1935939fd19091
Adrian <klimasa98@gmail.com>
2023-10-15 20:51:14 -0400

    created logic for game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 const box = document.querySelector('.box');
+const redBtn = document.querySelector('.red');
+const greenBtn = document.querySelector('.green');
+const blueBtn = document.querySelector('.blue');
+const mixBtn = document.querySelector('.mix');
+const colorBtns = document.querySelectorAll('.colorBtn');
+const saturation = document.querySelector('.slider');
+const saturationVal = document.querySelector('.saturationValue');
+
+let redR;
+let redG;
+let redB;
+let choosedColor
+
+
 
 const fillBox = () => {
 	for (let i = 0; i < 729; i++) {
@@ -8,16 +22,23 @@ const fillBox = () => {
 	}
 };
 
-fillBox()
+fillBox();
 
-box.addEventListener('mousemove', (e)=>{
 
-    let x = 0
-    redR = Math.floor(Math.random()*255)
-    redG = Math.floor(Math.random()*100)
-    redB = Math.floor(Math.random()*100)
-    e.target.classList.contains('square') ?     e.target.style.backgroundColor = `rgb(${redR},${redG},${redB})` : ''
+const generateRandColor = () => {
 
-   
-   
-})
+    let x = Math.floor(Math.random()*225 + 30)
+    choosedColor = `rgb(${x},0,0, .${(saturation.value)})`
+
+}
+
+
+
+
+box.addEventListener('mousemove', e => {
+    generateRandColor()
+	e.target.classList.contains('square') ? (e.target.style.backgroundColor = `${choosedColor}`) : '';
+});
+saturation.oninput = function () {
+	saturationVal.innerHTML = `${this.value}%`;
+};
