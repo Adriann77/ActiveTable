5a19c00067daaae677b27d5258092170de57bdac
Adrian <klimasa98@gmail.com>
2023-10-17 20:19:13 -0400

    optimalize code

--- js/script.js
+++ js/script.js
@@ -4,78 +4,62 @@ const saturation = document.querySelector('.slider');
 const saturationVal = document.querySelector('.saturationValue');
 const fadeSpeed = document.querySelectorAll('.speedBtn');
 let currColor;
-let currSpeed;
-currSpeed = 'Normal';
-currColor = 'mix';
+
+let h = 360;
+let currSpeed = '200';
 
 const fillBox = () => {
-	for (let i = 0; i < 729; i++) {
+	for (let i = 0; i < 722; i++) {
 		const square = document.createElement('div');
 		square.classList.add('square');
 		box.appendChild(square);
 	}
+
+	box.addEventListener('mouseover', e => {
+		e.target.classList.contains('square') ? generateColor(e.target) : '';
+	});
+
+	fadeSpeed.forEach(el =>
+		el.addEventListener('click', () => {
+			currSpeed = el.value;
+		})
+	);
+
+	colorBtns.forEach(el =>
+		el.addEventListener('click', () => {
+			h = el.value;
+		})
+	);
+};
+
+const generateColor = square => {
+	saturation.oninput = function () {
+		saturationVal.innerHTML = `${this.value}%`;
+	};
+	let s = saturation.value + '%';
+
+	if (h === '360' || h === 360) {
+		square.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)},${s}, 50%)`;
+	} else {
+		square.style.backgroundColor = `hsl(${Math.floor(Math.random() * 55 + parseInt(h))},${s}, 50%)`;
+	}
+
+	if (currSpeed === '100') {
+		setTimeout(() => {
+			square.style.transition = 'background-Color .5s';
+			square.style.backgroundColor = 'transparent';
+		}, 1000);
+	} else if (currSpeed === '200') {
+		setTimeout(() => {
+			square.style.transition = 'background-Color 1.5s';
+			square.style.backgroundColor = 'transparent';
+		}, 3000);
+	} else if (currSpeed === '300') {
+		setTimeout(() => {
+			square.style.transition = 'background-Color 3.0s';
+			square.style.backgroundColor = 'transparent';
+		}, 9000);
+	}
 };
 
 fillBox();
-
-const generateColor = () => {
-	let x = Math.floor(Math.random() * 225 + 30);
-	let z = Math.floor(Math.random() * 225 + 30);
-	let y = Math.floor(Math.random() * 225 + 30);
-	choosedColor = `rgb(${z},${y},${x}, .${saturation.value})`;
-
-	switch (currColor.textContent) {
-		case 'Red':
-			choosedColor = `rgb(${x},0,0, .${saturation.value})`;
-			break;
-		case 'Green':
-			choosedColor = `rgb(0,${x},0, .${saturation.value})`;
-			break;
-		case 'Blue':
-			choosedColor = `rgb(0,0,${x}, .${saturation.value})`;
-			break;
-		case 'Mix':
-			choosedColor = `rgb(${z},${y},${x}, .${saturation.value})`;
-			break;
-	}
-};
-
-const fadingSquare = el => {
-	switch (currSpeed) {
-		case 'Fast':
-        setTimeout(()=>{
-            el.target.classList.contains('square') ? el.target.style.backgroundColor = '#333' : ''
-        },1000)
-			break;
-		case 'Normal':
-            setTimeout(()=>{
-                el.target.classList.contains('square') ? el.target.style.backgroundColor = '#333' : ''
-            },3000)
-			break;
-		case 'Slow':
-            setTimeout(()=>{
-                el.target.classList.contains('square') ? el.target.style.backgroundColor = '#333' : ''
-            },10000)
-			break;
-	}
-};
-saturation.oninput = function () {
-	saturationVal.innerHTML = `${this.value}%`;
-};
-
-fadeSpeed.forEach(el =>
-	el.addEventListener('click', () => {
-		currSpeed = el.textContent;
-	})
-);
-
-colorBtns.forEach(el =>
-	el.addEventListener('click', () => {
-		currColor = el;
-	})
-);
-box.addEventListener('mousemove', e => {
-	generateColor();
-	fadingSquare(e);
-	e.target.classList.contains('square') ? (e.target.style.backgroundColor = `${choosedColor}`) : '';
-});
